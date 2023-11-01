0815e225e6fa452e1c7be790b4f39e16d6d572a0
RevathiGuduru123 <RevathiGuduru123@users.noreply.github.com>
2023-10-31 21:06:39 -0500

    added

--- app.js
+++ app.js
@@ -8,7 +8,7 @@ var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 var GadgetsRouter = require('./routes/Gadgets');
 var boardRouter = require('./routes/board');
-var boardRouter = require('./routes/choose');
+var chooseRouter = require('./routes/choose');
 
 var app = express();
 
